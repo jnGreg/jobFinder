224ba9723e2c5d1b70c6fb2862c7332b196a1c83
jnGreg <janpgregor@gmail.com>
2022-05-31 11:12:25 +0200

    fixed last crawl info

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -5,11 +5,11 @@
 <a>authors' page: </a><a style="margin-right: 25px; color: indigo;" href = "https://github.com/jnGreg/jobFinder"> GitHub  </a>
 <?php require_once ('includes/config.inc.php'); ?>
 <a>scrapped site: </a><a style="color: indigo;" href = "https://justjoin.it/"> JustJoinIT  </a><br><br>
-<a style="margin-right: 50px;">Last scrapped: 22-05-2022 1:11 PM </a><a> Offers obtained: <?php $query = $conn->prepare("SELECT * FROM offers;");
-														$query->execute();
-														$query->store_result();
-														$num_offers = $query->num_rows;
-														echo $num_offers; ?>
-</a><br><br>
+<a style="margin-right: 50px;">Last scrapped: <?php $query_last = "SELECT * FROM crawler_status ORDER BY time_parsed DESC LIMIT 1;";
+										$results_last = mysqli_query($conn, $query_last); 
+										while($row = mysqli_fetch_array($results_last)) {
+										$last = $row['time_parsed'];
+										$num_offers = $row['active_len'];
+										echo $last . '&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspOffers obtained: ' . $num_offers; }?> </a><br><br><br>
 </div>
 </center>
